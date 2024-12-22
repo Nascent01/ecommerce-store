63e8cc48f1cd8d519b638348e04d0eec7dc8bb51
Nascent <nasko6670@abv.bg>
2024-12-22 16:39:09 +0200

    Refactor ImportProducts command to use CommandTrait and update command signature and description

--- app/Console/Commands/Products/ImportProducts.php
+++ app/Console/Commands/Products/ImportProducts.php
@@ -3,28 +3,36 @@
 namespace App\Console\Commands\Products;
 
 use Illuminate\Console\Command;
+use App\Traits\CommandTrait;
 
 class ImportProducts extends Command
 {
+    use CommandTrait;
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'app:import-products';
+    protected $signature = 'command:import-products';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Import products data from Json file';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $scriptTimeStart = $this->displayCommandStart('Importing products data has started...');
+        $this->truncateTables(['products', 'product_translations', 'product_categories', 'product_category_translations']);
+
+        // Import products data from Json file
+
+        $this->displayExecutionTime($scriptTimeStart);
     }
 }
