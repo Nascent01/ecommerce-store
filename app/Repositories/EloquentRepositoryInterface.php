1ae142d6a94d20aba58297cdda64f5c6d71cc47f
Nascent <nasko6670@abv.bg>
2024-12-26 11:37:41 +0200

    Add EloquentRepositoryInterface for repository pattern implementation

---
+++ app/Repositories/EloquentRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Interface EloquentRepositoryInterface
+ * @package App\Repositories
+ */
+interface EloquentRepositoryInterface
+{
+    public function find($id): ?Model;
+}
