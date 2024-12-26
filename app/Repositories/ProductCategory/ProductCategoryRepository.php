b152c11d6e29427cf71fa138c205ddfbe4d46ec3
Nascent <nasko6670@abv.bg>
2024-12-26 11:37:48 +0200

    Add ProductCategoryRepository for managing product categories

---
+++ app/Repositories/ProductCategory/ProductCategoryRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repositories\ProductCategory;
+
+use App\Models\ProductCategory\ProductCategory;
+use App\Models\ProductCategory\ProductCategoryTranslation;
+use App\Repositories\EloquentRepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategoryRepository implements EloquentRepositoryInterface
+{
+    /**
+     * Find a product category by ID.
+     *
+     * @param $id
+     * @return Model|null
+     */
+    public function find($id): ?Model
+    {
+        return ProductCategory::find($id);
+    }
+
+    public function findByName(string $name): ?Model
+    {
+        return ProductCategoryTranslation::where('name', $name)->first();
+    }
+}
