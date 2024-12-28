6c6737a5306b5f54ee68f62074f7d70b633d132a
Nascent <nasko6670@abv.bg>
2024-12-28 16:47:10 +0200

    Remove findByName method from AttributeRepository

--- app/Repositories/Attribute/AttributeRepository.php
+++ app/Repositories/Attribute/AttributeRepository.php
@@ -2,7 +2,6 @@
 
 namespace App\Repositories\Attribute;
 
-use App\Models\Attribute\AttributeTranslation;
 use App\Models\Attribute\Attribute;
 use App\Repositories\EloquentRepositoryInterface;
 use Illuminate\Database\Eloquent\Model;
@@ -18,10 +17,5 @@ class AttributeRepository implements EloquentRepositoryInterface
     public function find($id): ?Model
     {
         return Attribute::find($id);
-    }
-
-    public function findByName(string $name): ?Model
-    {
-        return AttributeTranslation::where('name', $name)->first();
     }
 }
