bf961465a9ea6e9a674fa4ac03c506c304c7cf4d
Nascent <nasko6670@abv.bg>
2024-12-28 13:13:54 +0200

    Add color and size identifiers to products table

--- database/migrations/2024_12_22_132257_create_products_table.php
+++ database/migrations/2024_12_22_132257_create_products_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('sku')->index();
+            $table->string('color_identifier')->nullable();
+            $table->string('size_identifier')->nullable();
             $table->timestamps();
 
             $table->unique(['sku', 'id']);
